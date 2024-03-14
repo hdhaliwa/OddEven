f13d290bc1835f20f36dcb3a3435ae64d15986cf
hdhaliwa <hdhaliwa@asu.edu>
2024-03-14 10:27:16 -0700

    first commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int n = 11;
+        if(n % 2 == 0 ) {
+            System.out.print("Entered number is even");
+        } else {
+            System.out.print("Entered number is odd");
+        }
+    }
+}
